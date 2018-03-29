1817a3e2b3d9db698681f8953684a5bfaeb49fb5
dannypsnl <dannypsnl@gmail.com>
2018-03-30 02:40:13 +0800

    add: impl `TkValue` & `Token`

---
+++ src/lexer.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, PartialEq)]
+enum TkValue {
+    Int(i32),
+}
+
+#[derive(Debug, PartialEq)]
+struct Token((u32, u32), TkValue);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compare_tkvalue() {
+        assert_eq!(TkValue::Int(10), TkValue::Int(10));
+    }
+
+    #[test]
+    fn compare_token() {
+        assert_eq!(
+            Token((0, 0), TkValue::Int(3)),
+            Token((0, 0), TkValue::Int(3))
+        );
+    }
+}
