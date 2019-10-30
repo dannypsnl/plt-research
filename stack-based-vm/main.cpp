5e306c03a8ae3cfbe4a1112870d919141155781a
dannypsnl <dannypsnl@gmail.com>
2019-10-30 22:13:55 +0800

    format

--- stack-based-vm/main.cpp
+++ stack-based-vm/main.cpp
@@ -1,6 +1,6 @@
+#include <array>
 #include <iostream>
 #include <vector>
-#include <array>
 
 class StackOverflowException : std::exception {};
 class UnknownOpcodeException : std::exception {};
@@ -14,14 +14,13 @@ union VariantValue {
   int i;
 };
 
-class Value{
- private:
+class Value {
+private:
   ValueType _value_type;
   VariantValue v;
 
- public:
-
-  static Value* Int(int i) {
+public:
+  static Value *Int(int i) {
     auto v = new Value;
     v->_value_type = ValueType ::Int;
     v->v.i = i;
@@ -35,7 +34,7 @@ class Value{
     return this->v.i;
   }
 
- private:
+private:
 };
 
 enum OpCode {
@@ -47,55 +46,56 @@ enum OpCode {
 };
 
 struct Instruction {
-  explicit Instruction(OpCode opcode): _opcode{opcode} {}
-  Instruction(OpCode opcode, Value* operand): _opcode{opcode} , _operand{operand} {}
+  explicit Instruction(OpCode opcode) : _opcode{opcode} {}
+  Instruction(OpCode opcode, Value *operand)
+      : _opcode{opcode}, _operand{operand} {}
 
   OpCode _opcode;
-  Value* _operand;
+  Value *_operand;
 };
 
 #define STACK_MAX 1024
-#define BINARY_OP() \
-  auto l = pop()->get_int(); \
+#define BINARY_OP()                                                            \
+  auto l = pop()->get_int();                                                   \
   auto r = pop()->get_int();
 
 class VM {
- public:
-  VM(std::initializer_list<Instruction> is): _instructions{is} {}
+public:
+  VM(std::initializer_list<Instruction> is) : _instructions{is} {}
 
   void run() {
     for (auto ins : _instructions) {
       switch (ins._opcode) {
-        case OP_PUSH:
-          push(ins._operand);
-          break;
-        case OP_ADD: {
-          BINARY_OP()
-          push(Value::Int(l + r));
-          break;
-        }
-        case OP_SUB: {
-          BINARY_OP()
-          push(Value::Int(l - r));
-          break;
-        }
-        case OP_MUL: {
-          BINARY_OP()
-          push(Value::Int(l * r));
-          break;
-        }
-        case OP_DIV: {
-          BINARY_OP()
-          push(Value::Int(l / r));
-          break;
-        }
-        default:
-          throw new UnknownOpcodeException();
+      case OP_PUSH:
+        push(ins._operand);
+        break;
+      case OP_ADD: {
+        BINARY_OP()
+        push(Value::Int(l + r));
+        break;
+      }
+      case OP_SUB: {
+        BINARY_OP()
+        push(Value::Int(l - r));
+        break;
+      }
+      case OP_MUL: {
+        BINARY_OP()
+        push(Value::Int(l * r));
+        break;
+      }
+      case OP_DIV: {
+        BINARY_OP()
+        push(Value::Int(l / r));
+        break;
+      }
+      default:
+        throw new UnknownOpcodeException();
       }
     }
   }
 
-  Value* pop() {
+  Value *pop() {
     _stack_pointer--;
     return _stack[_stack_pointer];
   }
@@ -107,22 +107,23 @@ class VM {
     _stack_pointer++;
   }
 
- private:
+private:
   std::vector<Instruction> _instructions;
-  std::array<Value*, STACK_MAX> _stack{};
+  std::array<Value *, STACK_MAX> _stack{};
   int _stack_pointer = 0;
 };
 
 int main() {
-  VM vm {
-    Instruction(OP_PUSH, Value::Int(3)),
-    Instruction(OP_PUSH, Value::Int(1)),
-    Instruction(OP_PUSH, Value::Int(2)),
-    Instruction(OP_ADD),
-    Instruction(OP_DIV),
+  VM vm{
+      Instruction(OP_PUSH, Value::Int(3)),
+      Instruction(OP_PUSH, Value::Int(1)),
+      Instruction(OP_PUSH, Value::Int(2)),
+      Instruction(OP_ADD),
+      Instruction(OP_DIV),
   };
 
   vm.run();
-  std::cout << "VM Result: " << ", top of stack: " << vm.pop()->get_int() << std::endl;
+  std::cout << "VM Result: "
+            << ", top of stack: " << vm.pop()->get_int() << std::endl;
   return 0;
 }
