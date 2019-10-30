d24e9c6a742c11fac5a8a942fbdf265b19c80d49
dannypsnl <dannypsnl@gmail.com>
2019-10-30 22:09:20 +0800

    replace int with value, for future extend

--- stack-based-vm/main.cpp
+++ stack-based-vm/main.cpp
@@ -2,93 +2,127 @@
 #include <vector>
 #include <array>
 
-enum class OpResult {
-  OK,
-  FAIL,
+class StackOverflowException : std::exception {};
+class UnknownOpcodeException : std::exception {};
+class TypeMismatchingException : std::exception {};
+
+enum class ValueType {
+  Int,
+};
+
+union VariantValue {
+  int i;
+};
+
+class Value{
+ private:
+  ValueType _value_type;
+  VariantValue v;
+
+ public:
+
+  static Value* Int(int i) {
+    auto v = new Value;
+    v->_value_type = ValueType ::Int;
+    v->v.i = i;
+    return v;
+  }
+
+  int get_int() {
+    if (_value_type != ValueType::Int) {
+      throw new TypeMismatchingException();
+    }
+    return this->v.i;
+  }
+
+ private:
 };
 
 enum OpCode {
+  OP_PUSH,
   OP_ADD,
   OP_SUB,
   OP_MUL,
   OP_DIV,
 };
 
-std::string to_string(OpResult r) {
-  switch (r) {
-    case OpResult::OK:
-      return "OK";
-    case OpResult::FAIL:
-      return "FAIL";
-  }
-}
+struct Instruction {
+  explicit Instruction(OpCode opcode): _opcode{opcode} {}
+  Instruction(OpCode opcode, Value* operand): _opcode{opcode} , _operand{operand} {}
+
+  OpCode _opcode;
+  Value* _operand;
+};
 
 #define STACK_MAX 1024
 #define BINARY_OP() \
-  auto l = pop(); \
-  auto r = pop();
+  auto l = pop()->get_int(); \
+  auto r = pop()->get_int();
 
 class VM {
  public:
-  VM(std::initializer_list<int> is): _instructions{is} {}
+  VM(std::initializer_list<Instruction> is): _instructions{is} {}
 
-  OpResult run() {
-    for (auto i : _instructions) {
-      switch (i) {
+  void run() {
+    for (auto ins : _instructions) {
+      switch (ins._opcode) {
+        case OP_PUSH:
+          push(ins._operand);
+          break;
         case OP_ADD: {
           BINARY_OP()
-          push(l + r);
+          push(Value::Int(l + r));
           break;
         }
         case OP_SUB: {
           BINARY_OP()
-          push(l - r);
+          push(Value::Int(l - r));
           break;
         }
         case OP_MUL: {
           BINARY_OP()
-          push(l * r);
+          push(Value::Int(l * r));
           break;
         }
         case OP_DIV: {
           BINARY_OP()
-          push(l / r);
+          push(Value::Int(l / r));
           break;
         }
         default:
-          return OpResult::FAIL;
+          throw new UnknownOpcodeException();
       }
     }
-    return OpResult::OK;
   }
 
-  int pop() {
+  Value* pop() {
     _stack_pointer--;
-    auto r = _stack[_stack_pointer];
-    return r;
+    return _stack[_stack_pointer];
   }
-  OpResult push(int v) {
+  void push(Value *v) {
     if (_stack_pointer >= _stack.size()) {
-      return OpResult::FAIL;
+      throw new StackOverflowException();
     }
     _stack[_stack_pointer] = v;
     _stack_pointer++;
-    return OpResult::OK;
   }
 
  private:
-  std::vector<int> _instructions;
-  std::array<int, STACK_MAX> _stack{};
+  std::vector<Instruction> _instructions;
+  std::array<Value*, STACK_MAX> _stack{};
   int _stack_pointer = 0;
 };
 
 int main() {
-  VM vm {OP_ADD, OP_DIV};
-  vm.push(3);
-  vm.push(1);
-  vm.push(2);
+  VM vm {
+    Instruction(OP_PUSH, Value::Int(3)),
+    Instruction(OP_PUSH, Value::Int(1)),
+    Instruction(OP_PUSH, Value::Int(2)),
+    Instruction(OP_ADD),
+    Instruction(OP_DIV),
+  };
 
-  auto result = vm.run();
-  std::cout << "VM Result: " << to_string(result) << ", top of stack: " << vm.pop() << std::endl;
+  vm.run();
+  std::cout << "VM Result: " << ", top of stack: " << vm.pop()->get_int() << std::endl;
   return 0;
 }
