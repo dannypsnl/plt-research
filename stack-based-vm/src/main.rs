956c10fe8ea7c4899c1ed69efdda2986310f6ee3
dannypsnl <dannypsnl@gmail.com>
2019-11-09 18:32:48 +0800

    main

---
+++ stack-based-vm/src/main.rs
@@ -0,0 +1,18 @@
+use stack_based_vm::value::Value;
+use stack_based_vm::vm::{Instruction, ADD, DIV, PUSH, VM};
+use stack_based_vm::vm::Result;
+
+fn main() -> Result<()> {
+    let mut vm = VM::new();
+
+    vm.run(vec![
+        Instruction::op_code_and_operand(PUSH, Value::Int(3)),
+        Instruction::op_code_and_operand(PUSH, Value::Int(1)),
+        Instruction::op_code_and_operand(PUSH, Value::Int(2)),
+        Instruction::op_code(ADD),
+        Instruction::op_code(DIV),
+    ])?;
+
+    println!("top of stack: {:?}", vm.pop());
+    Ok(())
+}
