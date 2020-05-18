9c543b96edaadafdabaf3b3d595014c28f88e533
dannypsnl <dannypsnl@gmail.com>
2020-05-18 20:07:32 +0800

    hm: infer list, introduce type constructor and freevar

--- hindley-milner/hm-rkt/semantic.rkt
+++ hindley-milner/hm-rkt/semantic.rkt
@@ -3,12 +3,38 @@
 (require "lang.rkt"
          "typ.rkt")
 
-(: type/infer (-> expr typ))
-(define (type/infer exp)
+(struct Context
+  [(freevar-counter : Integer)]
+  #:transparent
+  #:mutable)
+(define (Context/new)
+  (Context 0))
+(: Context/freevar! (-> Context Integer))
+(define (Context/freevar! ctx)
+  (let ([cur-count (Context-freevar-counter ctx)])
+    (set-Context-freevar-counter! ctx (+ 1 (Context-freevar-counter ctx)))
+    cur-count))
+
+(: type/infer (->* (expr) (Context) typ))
+(define (type/infer exp [ctx (Context/new)])
   (match exp
     ;;; some expressiones are easy to guess what it is
     ; in our **langauge** is `int`, `bool` and `string`
     ([expr:int _] (typ:builtin "int"))
     ([expr:bool _] (typ:builtin "bool"))
     ([expr:string _] (typ:builtin "string"))
+    ;;; however there are some type is hard to guess, for example, `list`
+    ; list type takes a type parameter to construct a concerete type
+    ; we called it **type constructor**, we say `list a` is a type if `a` is a type
+    ; however, we have no idea what is `a`, until any application create a type
+    ; and we still have to store it, to express this concept, **type free variable** was introduced
+    ; It could be a thing like `list ?0`, and `?0` should be replaced with a type later
+    ; once `?0` bind to a type, we never change it
+    ; the tricky part in inference type of list is: depends on list has elements or not, freevar added or not needed.
+    ([expr:list elems]
+     (typ:constructor "list"
+                      (list (if (empty? elems)
+                                (typ:freevar (Context/freevar! ctx))
+                                ; use first element type as type of all elements
+                                (type/infer (car elems))))))
     (_ (raise "unimplemented yet"))))
--- hindley-milner/hm-rkt/typ.rkt
+++ hindley-milner/hm-rkt/typ.rkt
@@ -1,7 +1,14 @@
 #lang typed/racket
 
 (provide typ
-         typ:builtin)
+         typ:builtin
+         typ:freevar
+         typ:constructor)
 
 (struct typ [] #:transparent)
 (struct typ:builtin typ [(name : String)] #:transparent)
+(struct typ:freevar typ [(index : Integer)] #:transparent)
+(struct typ:constructor typ
+  [(name : String)
+   (arg : (Listof typ))]
+  #:transparent)
