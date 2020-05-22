399c46f268c37aef0cce895898c46ea1679eae71
dannypsnl <dannypsnl@gmail.com>
2020-05-22 10:53:18 +0800

    remove scala implementation

--- hindley-milner/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "hindley-milner"
-
-version := "0.1"
-
-scalaVersion := "2.13.1"
--- hindley-milner/src/main/scala/hm/main.scala
+++
@@ -1,7 +0,0 @@
-package hm
-
-object main extends App {
-  val typeChecker = new TypeChecker
-  val (typ, _) = typeChecker.inference(Lambda("x", Variable("x")))
-  println(typ)
-}
