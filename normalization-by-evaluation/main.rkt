e3c75a875b441439606af9eeff82b36374096d09
dannypsnl <dannypsnl@gmail.com>
2020-06-02 19:20:57 +0800

    nbe: subst

--- normalization-by-evaluation/main.rkt
+++ normalization-by-evaluation/main.rkt
@@ -9,6 +9,20 @@
 ;    | (λ (X) NF)
 ; NEU = X
 ;    | (NEU NF)
+;;; subst
+(define (subst e x s)
+  (match e
+    [`(λ (,xi-1) ,b)
+     (if (equal? `,xi-1 `,x)
+         `,e
+         `(λ (,xi-1)
+            ,(subst b x s)))]
+    [`(,e1 ,e2)
+     `(,(subst e1 x s) ,(subst e2 x s))]
+    [`,e
+     (if (equal? `,e `,x)
+         `,s
+         `,e)]))
 ;;; evaluate : T → T
 (define (evaluate e)
   (match e
