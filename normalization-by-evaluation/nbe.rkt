ba9ed8b22833d93b987a9ddf5ead69e09396b625
dannypsnl <dannypsnl@gmail.com>
2020-06-15 21:42:10 +0800

    nbe: α-equiv

--- normalization-by-evaluation/nbe.rkt
+++ normalization-by-evaluation/nbe.rkt
@@ -240,3 +240,51 @@
       #t
       #f))
 (define (var? x) (and (symbol? x) (not (keywords? x))))
+
+(define (α-equiv? e1 e2)
+  (α-equiv-aux e1 e2 '() '()))
+(define (α-equiv-aux e1 e2 xs1 xs2)
+  (match* (e1 e2)
+    [(kw kw) #:when (keyword? kw) #t]
+    [(x y)
+     ;;; both are variables
+     #:when (and (var? x) (var? y))
+     (match* ((assv x xs1) (assv y xs2))
+       [(#f #f) (eqv? x y)]
+       [((cons _ b1) (cons _ b2)) (eqv? b1 b2)]
+       [(_ _) #f])]
+    [(`(λ (,x) ,b1) `(λ (,y) ,b2))
+     (let ([fresh (gensym)])
+       ; λ introduces free var
+       (let ([bigger1 (cons (cons x fresh) xs1)]
+             [bigger2 (cons (cons y fresh) xs2)])
+         (α-equiv-aux b1 b2 bigger1 bigger2)))]
+    [(`(Π ((,x ,A1)) ,B1) `(Π ((,y ,A2)) ,B2))
+     (and (α-equiv-aux A1 A2 xs1 xs2)
+          ; Π introduces free type
+          (let ([fresh (gensym)])
+            (let ([bigger1 (cons (cons x fresh) xs1)]
+                  [bigger2 (cons (cons y fresh) xs2)])
+              (α-equiv-aux B1 B2 bigger1 bigger2))))]
+    [(`(Σ ((,x ,A1)) ,B1) `(Σ ((,y ,A2)) ,B2))
+     (and (α-equiv-aux A1 A2 xs1 xs2)
+          ; Σ introduces free type
+          (let ([fresh (gensym)])
+            (let ([bigger1 (cons (cons x fresh) xs1)]
+                  [bigger2 (cons (cons y fresh) xs2)])
+              (α-equiv-aux B1 B2 bigger1 bigger2))))]
+    [(`',x `',y)
+     (eqv? x y)]
+    ; This, together with read-back-norm, implements the η law for Absurd.
+    [(`(the Absurd ,e1) `(the Absurd e2))
+     #t]
+    [((cons op args1) (cons op args2))
+     #:when (keyword? op)
+     (and (= (length args1) (length args2))
+          (for/and ([arg1 (in-list args1)]
+                    [arg2 (in-list args2)])
+            (α-equiv-aux arg1 arg2 xs1 xs2)))]
+    [((list rator1 rand1) (list rator2 rand2))
+     (and (α-equiv-aux rator1 rator2 xs1 xs2)
+          (α-equiv-aux rand1 rand2 xs1 xs2))]
+    [(_ _) #f]))
