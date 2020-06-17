f1a2f7325353e80bb4a8b6e2c440383a37c01631
dannypsnl <dannypsnl@gmail.com>
2020-06-17 20:47:35 +0800

    λ∏: update git message template, prepare racket version

---
+++ lambda-pi/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "lambda-pi")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(dannypsnl))
