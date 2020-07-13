69c68c411096e892dd4e39c37ead8a07c80320f5
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-13 10:31:15 +0800

    drop minitt implementation

--- minitt/README.md
+++
@@ -1,5 +0,0 @@
-# minitt
-
-minitt implementation
-
-Paper Link: http://www.cse.chalmers.se/~bengt/papers/GKminiTT.pdf
--- minitt/info.rkt
+++
@@ -1,7 +0,0 @@
-#lang info
-(define collection "minitt")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(dannypsnl))
