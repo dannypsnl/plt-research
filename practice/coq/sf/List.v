e95788b4656b68dd8a77da8e99c73d7b7457ceb1
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-08-23 21:13:35 +0800

    more list

--- practice/coq/sf/List.v
+++ practice/coq/sf/List.v
@@ -39,5 +39,32 @@ Inductive natlist : Type :=
 
 Notation "x :: l" := (cons x l)
   (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Fixpoint repeat (n count : nat) : natlist :=
+  match count with
+  | 0 => []
+  | S count' => n :: (repeat n count')
+  end.
+
+Fixpoint length (l : natlist) : nat :=
+  match l with
+  | [] => 0
+  | h :: t => S (length t)
+  end.
+
+Fixpoint append (l1 l2 : natlist) : natlist :=
+  match l1 with
+  | [] => l2
+  | h :: t => h :: (append t l2)
+  end.
+
+Notation "x ++ y" := (append x y)
+  (at level 60, right associativity).
+
+
+Example test_app1 : [1;2;3] ++ [4;5;6] = [1;2;3;4;5;6].
+Proof. reflexivity. Qed.
 
 End NatList.
