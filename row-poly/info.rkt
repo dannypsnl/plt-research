89def3469d3d1fb34b28cbc471ada5f397a7f5f4
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-09-09 21:34:49 +0800

    [row-poly] init

---
+++ row-poly/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "row-poly")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/row-poly.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(dannypsnl))
---
+++ row-poly/scribblings/row-poly.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[row-poly
+                    racket/base]]
+
+@title{row-poly}
+@author{dannypsnl}
+
+@defmodule[row-poly]
+
+Package Description Here
