3d4f7c3285a79aff2a33175e1785501993cd09cd
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-10-13 17:49:42 +0800

    [haskell] list comp and tracing

--- practice/haskell/src/Lib.hs
+++ practice/haskell/src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Debug.Trace
+
+list1 :: [Int]
+list1 = [2 * x | x <- [1, 2, 3, 4, 5]]
+list2 :: [Int]
+list2 = [n * x | x <- [1, 2, 3, 4, 5], let n = 3]
+list3 :: [Int]
+list3 = [n * x | x <- [1, 2, 3, 4, 5], let n = 3, odd x]
+
+traceInt :: Int
+traceInt = trace "tracing" 1
