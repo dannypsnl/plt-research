ea0cc2ad5d012a2c315adeebc957433dfe116dcd
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-01-18 23:04:06 +0800

    begin-for-syntax lift phase

--- practice/racket-macro/main.rkt
+++ practice/racket-macro/main.rkt
@@ -1,9 +1,13 @@
-#lang racket/base
+#lang racket
 
-(module phase-mismatch-mod racket
-  (require syntax/parse (for-syntax syntax/parse))
+(require (for-syntax syntax/parse))
+
+(begin-for-syntax
+  ;; without begin-for-syntax, the define-syntax-class would define in phase 0(runtime). begin-for-syntax lift it to phase 1
   (define-syntax-class foo
-    (pattern (a b c)))
-  (define-syntax (macro stx)
-    (syntax-parse stx
-      [(_ f:foo) #'(+ f.a f.b f.c)])))
+    (pattern (a b c))))
+
+(define-syntax (macro stx)
+  (syntax-parse stx
+    ;; thus `f:foo` can reference to foo class
+    [(_ f:foo) #'(+ f.a f.b f.c)]))
