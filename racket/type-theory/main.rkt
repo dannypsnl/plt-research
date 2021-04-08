f5ffa80285440171231a4b0d298a3ba0bc1c3664
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-04-09 01:15:32 +0800

    expr

--- racket/type-theory/main.rkt
+++ racket/type-theory/main.rkt
@@ -3,4 +3,15 @@
 (require "data.rkt")
 
 (data expr
-      [var (s : String)])
+      [var (name : String)]
+      [abs (name : String) (body : expr)]
+      [app (fn : expr) (arg : expr)]
+      [pi (name : String) (e : expr) (body : expr)]
+      [type (level : Integer)]
+      [nat]
+      [zero]
+      [succ (n : expr)]
+      [rec (e1 : expr) (e2 : expr) (e3 : expr) (e4 : expr)]
+      [id (e1 : expr) (e2 : expr) (e3 : expr)]
+      [refl (e : expr)]
+      [J (e1 : expr) (e2 : expr) (e3 : expr) (e4 : expr) (e5 : expr) (e6 : expr)])
