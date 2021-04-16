969669e6e4a587bda6f8699c9f18b1979e1b5214
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-04-16 15:52:39 +0800

    complete value and vapp

--- racket/type-theory/main.rkt
+++ racket/type-theory/main.rkt
@@ -18,10 +18,24 @@
 
 (data value
       [vabs (lam : (-> value value))]
-      [vpi (v : value) (lam : (-> value value))])
+      [vpi (v : value) (lam : (-> value value))]
+      [vtype (level : Integer)]
+      [vnat]
+      [vzero]
+      [vsucc (n : value)]
+      [vid (v1 : value) (v2 : value) (v3 : value)]
+      [vrefl (v : value)]
+      [vneutral (n : neutral)])
 
 (data neutral
       [nvar (name : String)]
       [napp (fn : neutral) (arg : value)]
       [nrec (n : neutral) (v1 : value) (v2 : value) (v3 : value)]
       [nj (v1 : value) (v2 : value) (v3 : value) (v4 : value) (v5 : value) (n : neutral)])
+
+(: vapp : value value -> value)
+(define (vapp u v)
+  (match u
+    [(vabs lam) (lam v)]
+    [(vneutral n) (vneutral (napp n v))]
+    [_ (error 'fail "u: ~a, v: ~a" u v)]))
