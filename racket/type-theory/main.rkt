8000f4938971591826fac4fd50cbddbb7a99bd6f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-04-16 15:47:11 +0800

    neutral

--- racket/type-theory/main.rkt
+++ racket/type-theory/main.rkt
@@ -15,3 +15,13 @@
       [id (e1 : expr) (e2 : expr) (e3 : expr)]
       [refl (e : expr)]
       [J (e1 : expr) (e2 : expr) (e3 : expr) (e4 : expr) (e5 : expr) (e6 : expr)])
+
+(data value
+      [vabs (lam : (-> value value))]
+      [vpi (v : value) (lam : (-> value value))])
+
+(data neutral
+      [nvar (name : String)]
+      [napp (fn : neutral) (arg : value)]
+      [nrec (n : neutral) (v1 : value) (v2 : value) (v3 : value)]
+      [nj (v1 : value) (v2 : value) (v3 : value) (v4 : value) (v5 : value) (n : neutral)])
