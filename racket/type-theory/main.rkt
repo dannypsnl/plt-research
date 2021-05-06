489801edcff1d7f4c51c1a69d0d4689bf2a1cadb
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-07 07:54:47 +0800

    check/infer

--- racket/type-theory/main.rkt
+++ racket/type-theory/main.rkt
@@ -33,6 +33,9 @@
       [nrec (n : neutral) value value value]
       [nj value value value value value (n : neutral)])
 
+(define-type Env (Immutable-HashTable Symbol value))
+(define-type TypeEnv (Immutable-HashTable Symbol value))
+
 (: vapp : value value -> value)
 (define (vapp u v)
   (match u
@@ -40,11 +43,15 @@
     [(vneutral n) (vneutral (napp n v))]
     [_ (error 'fail "u: ~a, v: ~a" u v)]))
 
-(: eval : (Immutable-HashTable Symbol value) expr -> value)
+(: var : Symbol -> value)
+(define (var s)
+  (vneutral (nvar s)))
+
+(: eval : TypeEnv expr -> value)
 (define (eval env t)
   (match t
     [(Var name) (hash-ref env name
-                          (λ () (vneutral (nvar name))))]
+                          (λ () (var name)))]
     [(abs x e) (vabs (λ (v) (eval (hash-set env x v) e)))]
     [(app fn arg) (vapp (eval env fn) (eval env arg))]
     [(pi x a e) (vpi (eval env a) (λ (v) (eval (hash-set env x v) e)))]
@@ -87,10 +94,10 @@
 (define/match (readback v)
   [((vabs f))
    (let ([x (gensym 'k)])
-     (abs x (readback (f (vneutral (nvar x))))))]
+     (abs x (readback (f (var x)))))]
   [((vpi a b))
    (let ([x (gensym)])
-     (pi x (readback a) (readback (b (vneutral (nvar x))))))]
+     (pi x (readback a) (readback (b (var x)))))]
   [((vtype i)) (type i)]
   [((vnat)) (nat)]
   [((vzero)) (zero)]
@@ -102,3 +109,92 @@
 (: veq : value value -> Boolean)
 (define (veq t u)
   (equal? (readback t) (readback u)))
+
+(: infer : TypeEnv Env expr -> value)
+(define (infer tenv env e)
+  (match e
+    [(var x) (hash-ref tenv
+                       (λ () (error 'unbound-variable "~a" x)))]
+    [(app t u)
+     (match (infer tenv env t)
+       [(vpi a b)
+        (check tenv env u a)
+        (b (eval env u))]
+       [_ (error 'not-appliable)])]
+    [(pi x a b)
+     (let* ([i (universe tenv env a)]
+            [a (eval env a)]
+            [j (universe (hash-set tenv x a) env b)])
+       (vtype (max i j)))]
+    [(type i) (vtype (add1 i))]
+    [(nat) (vtype 0)]
+    [(zero) (vnat)]
+    [(succ t)
+     (check tenv env t (vnat))
+     (vnat)]
+    [(rec n a z s)
+     (check tenv env n (vnat))
+     (match (eval env a)
+       [(vpi (vnat) a)
+        (let ([n (eval env n)])
+          (check tenv env z (a (vzero)))
+          (check tenv env s
+                 (vpi (vnat) (λ (n) (varr (a n) (a (vsucc n))))))
+          (a n))]
+       [_ (error 'type-error)])]
+    [(id a t u)
+     (let ([i (universe tenv env a)]
+           [a (eval env a)])
+       (check tenv env t a)
+       (check tenv env u a)
+       (vtype i))]
+    [(refl t)
+     (let ([a (infer tenv env t)]
+           [t (eval env t)])
+       (vid a t t))]
+    [(J a p r t u e)
+     (let ([i (universe tenv env a)]
+           [a (eval env a)])
+       (check tenv env p
+              (vpi a (λ (x)
+                       (vpi a (λ (y)
+                                (varr (vid a x y) (vtype i)))))))
+       (let ([p1 (eval env p)])
+         (: p : value value value -> value)
+         (define (p x y e) (vapp (vapp (vapp p1 x) y) e))
+         (check tenv env r (vpi a (λ (x) (p x x (vrefl x)))))
+         (check tenv env t a)
+         (check tenv env u a)
+         (let ([t (eval env t)]
+               [u (eval env u)])
+           (check tenv env e (vid a t u))
+           (p t u (eval env e)))))]
+    [(abs _ _) (error 'type-error)]))
+
+(: varr : value value -> value)
+(define (varr a b)
+  (vpi a (λ (_) b)))
+
+(: universe : TypeEnv Env expr -> Integer)
+(define (universe tenv env t)
+  (match (infer tenv env t)
+    [(vtype i) i]
+    [else (error 'type-error)]))
+
+(: check : TypeEnv Env expr value -> Void)
+(define (check tenv env t a)
+  (match* {t a}
+    [{(abs x t) (vpi a b)}
+     (let ([y (var (gensym 'k))])
+       (check (hash-set tenv x a)
+              (hash-set env x y)
+              t
+              (b y)))]
+    [{(refl t) (vid _ u v)}
+     (let ([t (eval env t)])
+       (unless (and (veq t u)
+                    (veq t v))
+         (error 'type-error)))]
+    [{t a}
+     (unless (veq a (infer tenv env t))
+       (error 'type-error))]))
