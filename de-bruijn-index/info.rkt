002500014921e3460394fe44793b0f53fb4d14f8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-07-26 15:56:38 +0800

    cleanup

--- de-bruijn-index/info.rkt
+++ de-bruijn-index/info.rkt
@@ -1,8 +1,7 @@
 #lang info
 (define collection "de-bruijn-index")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/de-bruijn-index.scrbl" ())))
+(define build-deps '("racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(dannypsnl))
