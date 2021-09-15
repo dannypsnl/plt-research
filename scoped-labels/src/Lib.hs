f4113add3c4a20d9cbf43c54b5aacebbb9d5e463
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-09-15 23:05:54 +0800

    check function

--- scoped-labels/src/Lib.hs
+++ scoped-labels/src/Lib.hs
@@ -1,7 +1,26 @@
 module Lib () where
 
-data Type
+import Control.Monad.Exception.Synchronous
 
-data Term
+data Type =
+  TyRecord [(String, Type)]
+  | TyTuple [Type]
+  | TyString
+  | TyInt
+
+data Term =
+  TmRecord [(String, Term)]
+  | TmTuple [Term]
+  | TmString String
+  | TmInt Integer
+
+data TypeError = TypeMismatched
+check :: Type -> Term -> Exceptional TypeError ()
+check (TyRecord s) (TmRecord ss)      = Success ()
+-- FIXME: check tuple
+check (TyTuple types) (TmTuple terms) = Success ()
+check TyString (TmString _)           = Success ()
+check TyInt (TmInt _)                 = Success ()
+check ty tm                           = throw TypeMismatched
 
 data Value
