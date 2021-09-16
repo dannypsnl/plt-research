8e6069539eaea20a73e805ac344ed6a6d8feb423
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-09-17 00:12:29 +0800

    scoped label: use Map

--- scoped-labels/src/ScopedLabel.hs
+++ scoped-labels/src/ScopedLabel.hs
@@ -1,15 +1,16 @@
 module ScopedLabel () where
 
 import Control.Monad.Exception.Synchronous
+import qualified Data.Map.Strict as M
 
 data Type
-  = TyRecord [(String, Type)]
+  = TyRecord (M.Map String Type)
   | TyTuple [Type]
   | TyString
   | TyInt
 
 data Term
-  = TmRecord [(String, Term)]
+  = TmRecord (M.Map String Term)
   | TmTuple [Term]
   | TmString String
   | TmInt Integer
@@ -18,14 +19,13 @@ data TypeError = TypeMismatched Type Type
 
 check :: Type -> Term -> Exceptional TypeError ()
 check (TyRecord s) (TmRecord ss) = Success ()
--- FIXME: check tuple
-check (TyTuple types) (TmTuple terms) = Success ()
+check (TyTuple types) (TmTuple terms) = zipWith check types terms
 check TyString (TmString _) = Success ()
 check TyInt (TmInt _) = Success ()
 check ty tm = throw $ TypeMismatched ty (typeof tm)
 
 typeof :: Term -> Type
-typeof (TmRecord terms) = TyRecord (map (\(s, term) -> (s, typeof term)) terms)
+typeof (TmRecord terms) = TyRecord (M.map typeof terms)
 typeof (TmTuple terms) = TyTuple (map typeof terms)
 typeof (TmString _) = TyString
 typeof (TmInt _) = TyInt
