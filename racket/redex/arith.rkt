08e7104ba8bb1814b65e995efa4e0f8e1c4153b9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-20 19:21:11 +0800

    judgement is unrequired

--- racket/redex/arith.rkt
+++ racket/redex/arith.rkt
@@ -14,17 +14,10 @@
 (define r
   (reduction-relation
    Arith #:domain E
-   (--> (O N_0 ...) N_1
-        (judgment-holds (Reduce (O N_0 ...) N_1))
-        "Reduce")))
-
-(define-judgment-form Arith
-  #:mode (Reduce I O)
-  #:contract (Reduce (O N ...) N)
-  [(Reduce (+ N ...) ,(apply * (term (N ...))))]
-  [(Reduce (* N ...) ,(apply * (term (N ...))))]
-  [(Reduce (sub1 N) ,(sub1 (term N)))]
-  [(Reduce (add1 N) ,(add1 (term N)))])
+   (--> (+ N ...) ,(apply * (term (N ...))))
+   (--> (* N ...) ,(apply * (term (N ...))))
+   (--> (add1 N) ,(add1 (term N)))
+   (--> (sub1 N) ,(sub1 (term N)))))
 
 (define -->r (compatible-closure r Arith E))
 (traces -->r (term (+ (add1 5) (* 2 3 4))))
