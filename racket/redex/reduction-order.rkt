68a896763db3756f581e2e1aa6cdc53a072f769d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-21 15:58:54 +0800

    extract lambda calculus as common foundation

--- racket/redex/reduction-order.rkt
+++ racket/redex/reduction-order.rkt
@@ -1,11 +1,7 @@
 #lang racket
-(require redex)
+(require redex
+         "lambda-calculus.rkt")
 
-(define-language LC
-  (E X
-     (λ (X ...) E)
-     (E E ...))
-  (X variable-not-otherwise-mentioned))
 (define-extended-language LC+number
   LC
   (E ::= ....
@@ -15,27 +11,6 @@
   (O O1 O-multi)
   (O1 add1 sub1)
   (O-multi + *))
-
-(define-metafunction LC
-  subst : ((any X) ...) any -> any
-  [(subst [(any_1 X_1) ... (any_X X) (any_2 X_2) ...] X) any_X]
-  [(subst [(any_1 X_1) ...]  x) x]
-  [(subst [(any_1 X_1) ...]  (lambda (X ...) any_body))
-   (lambda (X_new ...)
-     (subst ((any_1 X_1) ...)
-            (subst-raw ((X_new x) ...) any_body)))
-   (where  (X_new ...)  ,(variables-not-in (term any_body) (term (X ...))))]
-  [(subst [(any_1 X_1) ...]  (any ...)) ((subst [(any_1 X_1) ...]  any) ...)]
-  [(subst [(any_1 X_1) ...]  any_*) any_*])
-(define-metafunction LC
-  subst-raw : ((x x) ...) any -> any
-  [(subst-raw ((X_n1 X_o1) ... (X_new x) (X_n2 X_o2) ...) x) X_new]
-  [(subst-raw ((X_n1 X_o1) ...)  x) x]
-  [(subst-raw ((X_n1 X_o1) ...)  (lambda (X ...) any))
-   (lambda (X ...) (subst-raw ((X_n1 X_o1) ...)  any))]
-  [(subst-raw [(any_1 X_1) ...]  (any ...))
-   ((subst-raw [(any_1 X_1) ...]  any) ...)]
-  [(subst-raw [(any_1 X_1) ...]  any_*) any_*])
 
 (define r
   (reduction-relation
