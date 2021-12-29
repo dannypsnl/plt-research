13596281776b36a642a9f54b0e0fcb7287b0ea98
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-29 23:47:08 +0800

    contract

--- category/comonad.rkt
+++ category/comonad.rkt
@@ -1,13 +1,22 @@
 #lang racket
+(require racket/contract/parametric)
 
-(define (cutter n)
+(define/contract (cutter n)
+  (integer? . -> . (parametric->/c [A] ((listof A) . -> . (values (listof A) (listof A)))))
   (lambda (l)
     (split-at l n)))
-(define (condition n)
+(define/contract (condition n)
+  (integer? . -> . (parametric->/c [A] ((listof A) . -> . boolean?)))
   (lambda (l)
     (>= (length l) 2)))
 
 (define (f c? cut)
+  (parametric->/c
+   [A]
+   (((listof A) . -> . boolean?)
+    ((listof A) . -> . (values (listof A) (listof A)))
+    . -> .
+    (listof (listof A))))
   (define (f* l)
     (if (c? l)
         (let-values ([(l rest) (cut l)])
