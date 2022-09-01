3eef040f27f13e644f175c6ec4dd35812451fd2b
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-01 19:29:00 +0800

    foteus: init
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

---
+++ foteus/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "foteus")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(dannypsnl))
+(define license '(Apache-2.0 OR MIT))
