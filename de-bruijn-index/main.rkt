6dfa29633c92de4a3304f80430c53e30f1c4f97e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-01 18:52:28 +0800

    let indices count from inner to outer
    that's \x.\y.x is \\1 rather than \\0
    this is important that we can implement context easier
    
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- de-bruijn-index/main.rkt
+++ de-bruijn-index/main.rkt
@@ -1,87 +1,85 @@
 #lang typed/racket
 
-(module+ test
-  (require typed/rackunit))
+(define-type context (U empty ext))
+(struct empty () #:transparent)
+(struct ext ([v : value] [ctx : context]) #:transparent)
 
-(define-type term (U t:var t:λ (app term)))
-(define-type t:var Symbol)
-(struct t:λ
-  ([x : t:var]
-   [m : term])
-  #:transparent
+(define-type term (U S.var S.lam S.app))
+(struct S.var ([v : Nonnegative-Integer])
   #:property prop:custom-write
   (λ (v port mode)
-    (fprintf port "λ~a.~a"
-             (t:λ-x v)
-             (t:λ-m v))))
-(struct (t) app
-  ([t1 : t]
-   [t2 : t])
-  #:transparent
+    (fprintf port "#~a"
+             (S.var-v v)))
+  #:transparent)
+(struct S.lam ([binder : term])
+  #:property prop:custom-write
+  (λ (v port mode)
+    (fprintf port "λ.~a"
+             (S.lam-binder v)))
+  #:transparent)
+(struct S.app ([fn : term] [arg : term])
   #:property prop:custom-write
   (λ (v port mode)
     (fprintf port "(~a ~a)"
-             (app-t1 v)
-             (app-t2 v))))
+             (S.app-fn v)
+             (S.app-arg v)))
+  #:transparent)
 
-(define-type bterm (U b:var b:λ (app bterm)))
-(define-type b:var Integer)
-(struct b:λ ([m : bterm])
-  #:transparent
+(define-type value (U V.lam V.stuck))
+(define-type V.stuck (U V.var))
+(struct V.var ([v : Nonnegative-Integer])
   #:property prop:custom-write
   (λ (v port mode)
-    (fprintf port "λ~a" (b:λ-m v))))
+    (fprintf port "@~a"
+             (V.var-v v)))
+  #:transparent)
+(struct V.lam ([binder : term] [ctx : context])
+  #:property prop:custom-write
+  (λ (v port mode)
+    (fprintf port "λ.~a"
+             (V.lam-binder v)))
+  #:transparent)
 
-;;; convert lambda calculus to de-bruijn index form
-(: convert (->* (term)
-                ((Immutable-HashTable Symbol Integer))
-                bterm))
+(: proj : context Nonnegative-Integer -> value)
+(define (proj ctx i)
+  (match ctx
+    ; returns a stuck since we don't always have value in context in lambda calculus
+    [(empty) (V.var i)]
+    [(ext v ctx) (if (= i 0) v (proj ctx (sub1 i)))]))
+
+(: evaluate : context term -> value)
+(define (evaluate ctx t)
+  (cond
+    [(S.var? t) (proj ctx (S.var-v t))]
+    [(S.app? t)
+     (let ([v-fn (evaluate ctx (S.app-fn t))]
+           [v-arg (evaluate ctx (S.app-arg t))])
+       (cond
+         [(V.lam? v-fn) (evaluate (ext v-arg (V.lam-ctx v-fn))
+                                  (V.lam-binder v-fn))]
+         [else (error 'not-function "cannot apply ~a" v-fn)]))]
+    [(S.lam? t) (V.lam (S.lam-binder t) ctx)]))
+
+(: depth : Sexp -> Nonnegative-Integer)
+(define (depth t)
+  (match t
+    [`(λ (,x) ,body) (add1 (depth body))]
+    [`(,f ,a) (max (depth f) (depth a))]
+    [else 0]))
+(: convert : (->* (Sexp) ((Immutable-HashTable Symbol Nonnegative-Integer))
+                  term))
 (define (convert t [name->index (make-immutable-hash '())])
   (match t
-    ;; bind parameter name to an index and keep conversion
-    [(t:λ p b) (b:λ (convert b (hash-set name->index p (hash-count name->index))))]
-    [(app t1 t2) (app (convert t1 name->index) (convert t2 name->index))]
-    ;; get index from environment
-    [name (hash-ref name->index name)]))
+    [`(λ (,x) ,body)
+     (S.lam (convert body (hash-set name->index (cast x Symbol) (depth body))))]
+    [`(,t1 ,t2) (S.app (convert t1 name->index) (convert t2 name->index))]
+    [x (S.var (hash-ref name->index (cast x Symbol)))]))
 
 (module+ test
-  (check-equal? (convert (t:λ 'x 'x))
-                (b:λ 0))
-  (check-equal? (convert (t:λ 'f (app (t:λ 'x (app 'f (app 'x 'x)))
-                                      (t:λ 'x (app 'f (app 'x 'x))))))
-                (b:λ (app (b:λ (app 0 (app 1 1)))
-                          (b:λ (app 0 (app 1 1)))))))
+  (require typed/rackunit)
 
-;;; beta-reduction
-; if t1 is a λx.M then replace all x occurs in M with t2
-; since bterm using de-bruijn index, lifting after beta-reduction is required
-; to know which level we are, must count λ
-(: β-reduction (->* (bterm)
-                    (Integer (Immutable-HashTable Integer bterm))
-                    bterm))
-(define (β-reduction t [de-bruijn-level 0] [subst (make-immutable-hash '())])
-  (match t
-    [(b:λ body) (b:λ (β-reduction body (add1 de-bruijn-level) subst))]
-    [(app t1 t2)
-     (match t1
-       [(b:λ body)
-        (let ([reduced-term (β-reduction body (add1 de-bruijn-level)
-                                         (hash-set subst de-bruijn-level t2))])
-          ;;; dbi lifting by replace reduced-term (+ 1 dbi) with (var dbi)
-          (β-reduction reduced-term de-bruijn-level
-                       (hash-set subst (add1 de-bruijn-level) de-bruijn-level)))]
-       [_ (raise "cannot do application on non-lambda term")])]
-    [i (hash-ref subst i (λ () t))]))
-
-(module+ test
-  ;;; λx.x
-  ; => λ0
-  ; beta-> λ0
-  (check-equal? (β-reduction (convert (t:λ 'x 'x)))
-                (b:λ 0))
-  ;;; λx.(λy.λz.z x)
-  ; => λ(λλ2 0)
-  ; beta-> λλ2
-  ; beta-> λλ1 (de-bruijn index lifting)
-  (check-equal? (β-reduction (convert (t:λ 'x (app (t:λ 'y (t:λ 'z 'z)) 'x))))
-                (b:λ (b:λ 1))))
+  (check-equal? (evaluate (empty)
+                          (convert '(((λ (v) (λ (f) (f v))) k)
+                                     (λ (x) x))
+                                   (make-immutable-hash '((k . 100)))))
+                (V.var 100)))
